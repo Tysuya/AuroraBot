a07af8451cfaff688615ffae92e2abd5ded291e1
Tythor <tthorin@live.com>
2018-06-06 23:25:26 -0700

    AB

--- src/main/java/aurora/SpamFilter.java
+++ src/main/java/aurora/SpamFilter.java
@@ -11,20 +11,13 @@ public class SpamFilter {
     private static long time = System.currentTimeMillis() - 500;
 
     public static void spamFilter(MessageChannel channel, Message message) {
-        if (System.currentTimeMillis() - time < 500) {
-            message.delete().queue();
-            channel.sendMessage(message.getAuthor().getAsMention() + " Your message has been marked as spam and has been deleted.").complete().delete().queueAfter(5, TimeUnit.SECONDS);
-            time = System.currentTimeMillis();
-            return;
-        }
-
         List<Message> messageHistoryList = new MessageHistory(channel).retrievePast(10).complete();
         int counter = 0;
         for (Message eachMessage : messageHistoryList) {
             if (message.getAuthor().equals(eachMessage.getAuthor()) && message.getAttachments().isEmpty()) {
                 if (message.getContent().equals(eachMessage.getContent()))
                     counter++;
-                if (counter >= 3) {
+                if (counter >= 3 || System.currentTimeMillis() - time < 750) {
                     message.delete().queue();
                     channel.sendMessage(message.getAuthor().getAsMention() + " Your message has been marked as spam and has been deleted.").complete().delete().queueAfter(5, TimeUnit.SECONDS);
                     break;
--- src/main/java/aurora/commands/Delete.java
+++ src/main/java/aurora/commands/Delete.java
@@ -9,8 +9,16 @@ import java.util.List;
 public class Delete {
     public static void delete(MessageChannel channel, Message message) {
         int amount = Integer.parseInt(message.getContent().split("!delete ")[1]);
-        List<Message> messageHistoryList = new MessageHistory(channel).retrievePast(amount).complete();
-        for (Message eachMessage : messageHistoryList)
-            eachMessage.delete().queue();
+        int remainder = amount % 100;
+        int counter = (amount - remainder) / 100;
+        amount = 100;
+
+        for (int i = 0; i <= counter; i++) {
+            if (i == counter)
+                amount = remainder;
+            List<Message> messageHistoryList = new MessageHistory(channel).retrievePast(amount).complete();
+            for (Message eachMessage : messageHistoryList)
+                eachMessage.delete().queue();
+        }
     }
 }
